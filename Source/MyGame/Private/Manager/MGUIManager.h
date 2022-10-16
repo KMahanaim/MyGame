a3fa59e05eb70fc1a0c25f99114265710b4ec86c
KMahanaim <doogk3@naver.com>
2022-10-16 11:29:31 +0900

    #00 프로젝트 생성

---
+++ Source/MyGame/Private/Manager/MGUIManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MGUIManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UMGUIManager : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
